0b6fdabba339d891b76455f0682b9ce64fd423bf
Kutlay Akyurek <kutlay.akyurek@getir.com>
2022-02-21 20:48:14 +0300

    Change README file

--- src/main/java/com/ka/order/config/ModelMapperConfig.java
+++ src/main/java/com/ka/order/config/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.ka.order.config;
 
+import com.ka.order.converter.ContactConverter;
 import com.ka.order.converter.CustomerConverter;
+import com.ka.order.converter.OrderConverter;
+import com.ka.order.converter.ProductConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +12,19 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
 
     private final CustomerConverter customerConvertor;
+    private final ContactConverter contactConverter;
+    private final OrderConverter orderConverter;
+    private final ProductConverter productConverter;
 
-    public ModelMapperConfig(final CustomerConverter customerConvertor) {
+    public ModelMapperConfig(final CustomerConverter customerConvertor,
+                             final ContactConverter contactConverter,
+                             final OrderConverter orderConverter,
+                             final ProductConverter productConverter) {
+
         this.customerConvertor = customerConvertor;
+        this.contactConverter = contactConverter;
+        this.orderConverter = orderConverter;
+        this.productConverter = productConverter;
     }
 
 
@@ -19,6 +32,9 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         final ModelMapper mapper = new ModelMapper();
         mapper.addConverter(customerConvertor);
+        mapper.addConverter(contactConverter);
+        mapper.addConverter(orderConverter);
+        mapper.addConverter(productConverter);
 
         return mapper;
     }
